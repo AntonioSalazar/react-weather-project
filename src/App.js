ffe346774d1673420be34941fcb770340f52f42b
AntonioSE <antonio.salazar@oliverwyman.com>
2020-02-28 17:13:10 -0600

    the values the user select will be stored in the Form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
 
 function App() {
   return (
-    <div>
-      <h1>Working</h1>
-    </div>
+    <Fragment>
+      <Header 
+        title="Weather Forecast"
+      />
+      <div className='form-container'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col m6 s12'>
+              <Form
+                
+              />
+            </div>
+            <div className='col m6 s12'>
+              2
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+const Form = () => {
+
+
+    //The state that will keep user selection
+    const [ search, saveSearch ] = useState({
+        country: '',
+        city: ''
+    })
+    // //extracting the values from the state
+    const { country, city } = search;
+
+    //Function that will executed when the user make his selections
+
+    const handleChange = e => {
+        saveSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    return (
+        <form>
+            <div className='input-field col s10'>
+                <select
+
+                    name='country'
+                    id='country'
+                    value={country}
+                    onChange={handleChange}
+                >
+                    <option value='' disabled selected>Choose the Country</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                </select>
+                <label htmlFor='country'>Country</label>
+
+            </div>  
+            <div className='input-field col s10'>
+                <input 
+                    className='validate' 
+                    type='text' 
+                    id='city' 
+                    name='city'
+                    onChange={handleChange}/>
+                <label htmlFor='city' value={city}>City Name</label>
+            </div>    
+        </form>
+    )
+}
+
+export default Form;
