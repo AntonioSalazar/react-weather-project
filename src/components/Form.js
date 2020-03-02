120eb3ba2bc59e938b1237b743e7a56ffbc57ecc
AntonioSE <antonio.salazar@oliverwyman.com>
2020-03-02 10:51:12 -0600

    working on the form component, display an error message, added a submit btn

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,7 +7,9 @@ const Form = () => {
     const [ search, saveSearch ] = useState({
         country: '',
         city: ''
-    })
+    });
+
+    const [ error, setError ] = useState(false);
     // //extracting the values from the state
     const { country, city } = search;
 
@@ -18,10 +20,29 @@ const Form = () => {
             ...search,
             [e.target.name] : e.target.value
         })
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //Check if the fields are empty
+        if ( country.trim() === '' || city.trim() === '') {
+            setError(true);
+            return;
+        }
+
+        setError(false);
+
+        //Send the info to the app component
+
     }
 
     return (
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+
+        { error ? <p className='red darken-4 error custom-error'>All the fields are required</p> : null}
             <div className='input-field col s10'>
                 <select
                     name='country'
@@ -39,8 +60,8 @@ const Form = () => {
                     <option value="PE">Perú</option>
                 </select>
                 <label htmlFor='country'>Country</label>
-
             </div>  
+            
             <div className='input-field col s10'>
                 <input 
                     className='validate' 
@@ -49,7 +70,14 @@ const Form = () => {
                     name='city'
                     onChange={handleChange}/>
                 <label htmlFor='city' value={city}>City Name</label>
-            </div>    
+            </div>  
+            <div className='input-field col s10'>
+                <input 
+                    type='submit'
+                    className='waves-effect waves-light btn yellow accent-4'
+                    value='Get Weather!'
+                />
+            </div>
         </form>
     )
 }
